581fc5e76a3395a330e4b6a75c3d89d6de5142d1
USuck <yoUSuck.0327@gmail.com>
2017-03-21 21:05:15 +0900

    Added error plumber in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,11 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
+import plumber from 'gulp-plumber'
+import notify from 'gulp-notify'
 
 gulp.task('build', ()=>{
 	gulp.src('src/**/main.js')
+		.pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
 		.pipe(babel())
 		.pipe(gulp.dest('lib'))
 })
